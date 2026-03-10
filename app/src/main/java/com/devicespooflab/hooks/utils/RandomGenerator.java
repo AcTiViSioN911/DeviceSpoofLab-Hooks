1c0e83ee9555bc45b814548b7c33b6097b11097c
AcTiViSioN911 <theactivision@yandex.ru>
2026-03-10 04:26:11 -0400

    Update RandomGenerator.java

--- app/src/main/java/com/devicespooflab/hooks/utils/RandomGenerator.java
+++ app/src/main/java/com/devicespooflab/hooks/utils/RandomGenerator.java
@@ -7,25 +7,21 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.UUID;
 
-/**
- * Generates valid random device identifiers with proper checksums and formats.
- */
 public class RandomGenerator {
 
     private static final SecureRandom random = new SecureRandom();
 
     public static String generateIMEI() {
-        String tac = "35847631"; // Generic Google TAC
+        return generateIMEIWithTAC("35328461");
+    }
 
+    public static String generateIMEIWithTAC(String tac) {
         StringBuilder serial = new StringBuilder();
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 14 - tac.length(); i++) {
             serial.append(random.nextInt(10));
         }
-
         String imeiWithoutCheck = tac + serial.toString();
-        int checkDigit = calculateLuhnCheckDigit(imeiWithoutCheck);
-
-        return imeiWithoutCheck + checkDigit;
+        return imeiWithoutCheck + calculateLuhnCheckDigit(imeiWithoutCheck);
     }
 
     public static String generateMEID() {
@@ -38,44 +34,36 @@ public class RandomGenerator {
     }
 
     public static String generateIMSI() {
-        String mcc = "310";
-        String mnc = "260";
-
+        String mcc = "401";
+        String mnc = "02";
         StringBuilder msin = new StringBuilder();
         for (int i = 0; i < 10; i++) {
             msin.append(random.nextInt(10));
         }
-
         return mcc + mnc + msin.toString();
     }
 
     public static String generateICCID() {
-        String prefix = "8901";
-        String issuer = "260";
-
+        String prefix = "89701";
+        String issuer = "02";
         StringBuilder account = new StringBuilder();
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 12; i++) {
             account.append(random.nextInt(10));
         }
-
         String iccidWithoutCheck = prefix + issuer + account.toString();
-        int checkDigit = calculateLuhnCheckDigit(iccidWithoutCheck);
-
-        return iccidWithoutCheck + checkDigit;
+        return iccidWithoutCheck + calculateLuhnCheckDigit(iccidWithoutCheck);
     }
 
     public static String generatePhoneNumber() {
-        int areaCode = 200 + random.nextInt(800);
-        int exchange = 200 + random.nextInt(800);
-        int subscriber = random.nextInt(10000);
-
-        return String.format("+1%03d%03d%04d", areaCode, exchange, subscriber);
+        int prefix = random.nextBoolean() ? 701 : 702;
+        int subscriber = 1000000 + random.nextInt(9000000);
+        return String.format("+7%d%d", prefix, subscriber);
     }
 
     public static String generateSerial() {
-        String chars = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ"; // No I or O to avoid confusion
+        String chars = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ";
         StringBuilder serial = new StringBuilder();
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 12; i++) {
             serial.append(chars.charAt(random.nextInt(chars.length())));
         }
         return serial.toString();
@@ -109,69 +97,31 @@ public class RandomGenerator {
         return androidId.toString();
     }
 
-    /**
-     * Generate Build.FINGERPRINT in format:
-     * brand/product/device:version/build_id/incremental:type/keys
-     */
     public static String generateFingerprint() {
-        String buildId = generateBuildId();
-        String incremental = generateIncremental();
-        return String.format(
-            "google/cheetah/cheetah:15/%s/%s:user/release-keys",
-            buildId,
-            incremental
-        );
+        return "google/lynx/lynx:16/CP1A.260305.018/14887507:user/release-keys";
     }
 
-    /**
-     * Generate Build ID (e.g., AP4A.241205.013)
-     * Format: [A-Z]{2}[0-9]{1,2}[A-Z]\.YYMMDD\.XXX
-     */
     public static String generateBuildId() {
-        String prefix = "AP4A"; // Android 15 prefix
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyMMdd", Locale.US);
-        String date = dateFormat.format(new Date());
-        int build = random.nextInt(999) + 1;
-        return String.format("%s.%s.%03d", prefix, date, build);
+        return "CP1A.260305.018";
     }
 
-    /**
-     * Generate incremental build number (e.g., 12621605)
-     * 8-digit number
-     */
     public static String generateIncremental() {
-        return String.format("%08d", random.nextInt(100000000));
+        return "14887507";
     }
 
-    /**
-     * Generate bootloader version (e.g., cheetah-1.2-A1B2C3D4)
-     */
     public static String generateBootloader() {
         StringBuilder hex = new StringBuilder();
         String hexChars = "0123456789ABCDEF";
         for (int i = 0; i < 8; i++) {
             hex.append(hexChars.charAt(random.nextInt(16)));
         }
-        return "cheetah-1.2-" + hex.toString();
+        return "lynx-16.4-" + hex.toString();
     }
 
-    /**
-     * Generate security patch date (YYYY-MM-DD)
-     * Returns a recent date within last 90 days
-     */
     public static String generateSecurityPatch() {
-        Calendar cal = Calendar.getInstance();
-        // Random day within last 90 days
-        int daysAgo = random.nextInt(90);
-        cal.add(Calendar.DAY_OF_YEAR, -daysAgo);
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-        return dateFormat.format(cal.getTime());
+        return "2026-03-05";
     }
 
-    /**
-     * Generate random hex string of specified length
-     */
     public static String generateHex(int length) {
         StringBuilder hex = new StringBuilder();
         String hexChars = "0123456789abcdef";
@@ -184,21 +134,15 @@ public class RandomGenerator {
     private static int calculateLuhnCheckDigit(String number) {
         int sum = 0;
         boolean alternate = true;
-
         for (int i = number.length() - 1; i >= 0; i--) {
             int digit = Character.getNumericValue(number.charAt(i));
-
             if (alternate) {
                 digit *= 2;
-                if (digit > 9) {
-                    digit -= 9;
-                }
+                if (digit > 9) digit -= 9;
             }
-
             sum += digit;
             alternate = !alternate;
         }
-
         return (10 - (sum % 10)) % 10;
     }
 }
